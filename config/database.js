33a3546405d411cb830dc14a0e443a36a6df3a73
root <you@example.com>
2017-04-05 17:21:26 +0200

    add changes

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'zuzu'
+    },
+	'database': 'iich',
+    'users_table': 'users'
+};
